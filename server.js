421fa97b05d4a83e04a0be193a127c39d685ce0d
igprasad09 <vivek87228@gmail.com>
2026-06-24 21:15:54 +0530

    making vercel production level

--- server.js
+++ server.js
@@ -2,30 +2,28 @@ const express = require("express");
 const cors = require("cors");
 const pool = require("./db");
 const GeneratingProducts = require("./routes/generate");
+
 const app = express();
 
-app.use(cors({
-    origin: "http://localhost:5173"
-}))
+app.use(cors());
 
 app.use(express.json());
+
 app.use("/", GeneratingProducts);
 app.use("/", require("./routes/app"));
 
-
-app.get("/", (req, res)=>{
-    res.send("server is running");
+app.get("/", (req, res) => {
+    res.send("Server is running");
 });
 
-app.get("/test", async (req, res)=>{
-    try{
-        const result = await pool.query(`SELECT * FROM products`);
+app.get("/test", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM products");
         res.json(result.rows);
-    }catch(err){
+    } catch (err) {
         console.log(err);
+        res.status(500).json({ error: "Server Error" });
     }
-})
-
-app.listen(3000, ()=>{
-    console.log("Server Started.....");
 });
+
+module.exports = app;
